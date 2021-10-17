ed60b4636f50c96ecb9f11e82de92522d9006053
HowlandAlek <howlandalek@gmail.com>
2021-10-17 13:20:53 -0500

    Se agrega pantalla de login y se convierte en principal.

---
+++ app/src/main/java/mx/itesm/cerco/proyectofinal/Login.kt
@@ -0,0 +1,72 @@
+package mx.itesm.cerco.proyectofinal
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import mx.itesm.cerco.proyectofinal.databinding.ActivityLoginBinding
+
+class Login : AppCompatActivity() {
+
+    private val CODIGO_SIGNIN: Int = 500
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        configurarEventos()
+    }
+
+    private fun autenticar() {
+        val providers =
+            arrayListOf(AuthUI.IdpConfig.GoogleBuilder().build())
+
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),
+            CODIGO_SIGNIN
+        )
+    }
+
+    private fun configurarEventos() {
+        binding.btnSignin.setOnClickListener{
+            autenticar()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == CODIGO_SIGNIN) {
+            when (resultCode) {
+                RESULT_OK -> {
+                    val usuario =
+                        FirebaseAuth.getInstance().currentUser
+                    println("Bienvenido: ${usuario?.displayName}")
+                    println("Correo: ${usuario?.email}")
+                    println("Correo: ${usuario?.uid}")
+                    // Lanzar otra actividad
+                }
+                RESULT_CANCELED -> {
+                    println("Cancelado...")
+                    val response =
+                        IdpResponse.fromResultIntent(data)
+                    println("Error: ${response?.error?.localizedMessage}")
+                }
+                else -> {
+                    val response =
+                        IdpResponse.fromResultIntent(data)
+                    println("Error: ${response?.error?.errorCode}")
+                }
+            }
+        }
+    }
+
+}
