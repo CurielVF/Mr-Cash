a8ccee2705db59c9406ec329b2f22f495aba8963
FrancisJaGlez <a01748636@itesm.mx>
2021-10-21 21:22:59 -0500

    Se agregaron un par de Tips mas, y se ajusto el aspecto del cajon para que sea mas dinamico y mejor esteticamente.

--- app/src/main/java/mx/itesm/cerco/proyectofinal/ui/tips/model/ServicioTips.kt
+++ app/src/main/java/mx/itesm/cerco/proyectofinal/ui/tips/model/ServicioTips.kt
@@ -5,8 +5,16 @@ class ServicioTips {
     fun leerTips(): List<Tip> {
         return arrayListOf(
             Tip("8 Tips para ahorrar",
-                "El primer paso para comenzar a ahorrar dinero es determinar cuánto gasta..."
+                "Bank Of America nos regala 8 consejos..."
                         , "https://bettermoneyhabits.bankofamerica.com/es/saving-budgeting/ways-to-save-money"),
+            Tip("7 consejos básicos para finanzas personales",
+            "Entrepreneur nos da 7 consejos muy utiles...", "https://www.entrepreneur.com/article/268900"),
+            Tip("La Mejores SOFIPOS en 2021", "¿Quieres adentrarte mas sobre la educacion financiera?",
+                    "https://omareducacionfinanciera.com/mejores-sofipos-2021/"),
+            Tip("Los 10 Mejores ETF's para invertir", "¿Quieres invertir en bolsa? ve a lo seguro...",
+            "https://omareducacionfinanciera.com/mejores-sofipos-2021/"),
+            Tip("¿Cómo empezar a invertir en acciones?", "Bueno, hay que empezar con el pie derecho y tienes poca experiecia, bueno, no es necesaria",
+            "https://cnnespanol.cnn.com/2021/02/02/invertir-bolsa-sin-experiencia/")
 
 
         )
