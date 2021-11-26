34c3a3613c1fc4861329ad769330bf907aed3a44
Alek Howland <83378924+HowlandAlek@users.noreply.github.com>
2021-11-26 12:42:45 -0500

    Propuesta colores metas

--- app/src/main/java/mx/itesm/cerco/proyectofinal/ui/Constantes.kt
+++ app/src/main/java/mx/itesm/cerco/proyectofinal/ui/Constantes.kt
@@ -1,9 +1,9 @@
 package mx.itesm.cerco.proyectofinal.ui
 
 object Constantes {
-    const val color_completado = "#386641"
-    const val color_progreso = "#A7C957"
-    const val color_retraso = "#BC4749"
+    const val color_completado = "#8CB369"
+    const val color_progreso = "#F4A259"
+    const val color_retraso = "#BC4B51"
     const val color_unico = "#396491"
     const val color_mensual = "#39918C"
 }
